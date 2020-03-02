9c566030a4a52682beedd49dc6f43944c7292af9
wilmar52 <wflorez52@gmail.com>
2020-03-01 19:24:02 -0500

    Added Logo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfHeroCards } from './components/ListOfHeroCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfHeroCards />
   </>
 )
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 200px;
+  margin-left: -10px;
+  margin-top: -30px;
+`
